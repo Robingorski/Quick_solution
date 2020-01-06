9fa69556b41fc80f170f5d35fa603f14b01dca62
Robingorski <Robingorski@hotmail.com>
2020-01-06 17:16:55 +0100

    fixed scroll function when modal is up

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
   modal.style.display = "block";
+  body.style.overflow = "hidden";
+  html.style.overflow = "hidden";
 }
 
 span.onclick = function () {
   modal.style.display = "none";
+  body.style.overflow = "auto";
+  html.style.overflow = "auto";
 }
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none"; 
+    body.style.overflow = "auto";
+    html.style.overflow = "auto";
   }
 }
 
